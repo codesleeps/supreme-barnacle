c6319cfa2b86f73de4997096461c3c1489768d6a
codesleeps <125808990+codesleeps@users.noreply.github.com>
2023-05-16 09:16:50 +0100

    added employee engineer manger intern and added dir templates output

--- index.js
+++ index.js
@@ -10,6 +10,44 @@ const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./src/page-template.js");
 
-
 // TODO: Write Code to gather information about the development team members, and render the HTML file.
+
+const teamMembers = [];
+
+function start() {
+  inquirer.prompt([
+    {
+      type: "input",
+      name: "name",
+      message: "What is the team manager's name?",
+    },
+    {
+      type: "input",
+      name: "id",
+      message: "What is the team manager's ID?",
+    },
+    {
+        type: "input",
+        name: "email",
+        message: "What is the team manager's email?",
+    },
+    {
+        type: "input",
+        name: "officeNumber",
+        message: "What is the team manager's office number?",
+    },
+
+    
+  ])
+  .then((questions) => {
+    const manager = new Manager(questions.name, questions.id, questions.email, questions.officeNumber);
+    console.log(manager);
+    teamMembers.push(manager);
+    teamMembers();
+    console.log(teamMembers);
+
+
+  })
+}
+
 
